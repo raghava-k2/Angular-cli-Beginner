519f158848cce46710f441b9c044c137732118bc
vijaya,raghava <raghava.k2@gmail.com>
2017-06-27 16:13:24 +0530

    Modified Dao

--- src/server/dao/studentDao.js
+++ src/server/dao/studentDao.js
@@ -2,14 +2,19 @@ const sequelize = require('../util/dbUtil.js')
 const Sequelize = require('Sequelize');
 
 const StudentEntity = sequelize.define('students', {
-    id: Sequelize.NUMBER,
+    id: {
+        type: Sequelize.STRING,
+        primaryKey: true
+    },
     className: Sequelize.STRING,
     student_name: Sequelize.STRING
 });
 
 const studentDao = {
     selectStudentList: function (req, res) {
-        res.json(StudentEntity.findAll().then(data => { return data; }));
+        res.json(StudentEntity.findAll().then(data => {
+            return data;
+        }));
     }
 };
 module.exports = studentDao;
